c8702485914ddb4528ac14884d0237cd6bfede51
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2021-06-08 00:20:33 +0530

    volatile keyword example

--- src/com/codecafe/concurrency/_volatile/VolatileDemo.java
+++ src/com/codecafe/concurrency/_volatile/VolatileDemo.java
@@ -1,8 +1,41 @@
 package com.codecafe.concurrency._volatile;
 
+class Task implements Runnable {
+
+    volatile boolean isRunning = true;
+
+    @Override
+    public void run() {
+
+        while (isRunning) {
+            System.out.println(Thread.currentThread().getName() + " running");
+        }
+
+    }
+
+    public void shutdown() {
+        isRunning = false;
+        System.out.println("terminated by : " + Thread.currentThread().getName());
+    }
+
+}
+
 public class VolatileDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        Task task = new Task();
+
+        new Thread(task, "T1").start();
+        new Thread(task, "T2").start();
+
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        task.shutdown();
 
     }
 
