df02cff666bef6c0a4e3a4f716138c16c5042734
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2021-06-15 23:57:12 +0530

    atomic integer demo

---
+++ src/com/codecafe/concurrency/_atomic/AtomicIntegerRoundRobin.java
@@ -0,0 +1,25 @@
+package com.codecafe.concurrency._atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerRoundRobin {
+
+    private final int totalIndexes;
+    private final AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    public AtomicIntegerRoundRobin(int totalIndexes) {
+        this.totalIndexes = totalIndexes;
+    }
+
+    public synchronized int index() {
+
+        atomicInteger.compareAndSet(totalIndexes, 0);
+
+        int index = atomicInteger.get();
+
+        atomicInteger.getAndIncrement();
+
+        return index;
+    }
+
+}
---
+++ src/com/codecafe/concurrency/_atomic/Main.java
@@ -0,0 +1,31 @@
+package com.codecafe.concurrency._atomic;
+
+class Task implements Runnable {
+
+    private AtomicIntegerRoundRobin atomicIntegerRoundRobin;
+
+    public Task(AtomicIntegerRoundRobin atomicIntegerRoundRobin) {
+        this.atomicIntegerRoundRobin = atomicIntegerRoundRobin;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " - " + atomicIntegerRoundRobin.index());
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        AtomicIntegerRoundRobin atomicIntegerRoundRobin = new AtomicIntegerRoundRobin(3);
+
+        new Thread(new Task(atomicIntegerRoundRobin), "T1").start();
+        new Thread(new Task(atomicIntegerRoundRobin), "T2").start();
+        new Thread(new Task(atomicIntegerRoundRobin), "T3").start();
+        new Thread(new Task(atomicIntegerRoundRobin), "T4").start();
+        new Thread(new Task(atomicIntegerRoundRobin), "T5").start();
+    }
+
+}
