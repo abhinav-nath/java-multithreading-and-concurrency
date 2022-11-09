71d02071934bdf976ae9b13291f29fb2d38e4241
Abhinav Nath <abhinavnath@ymail.com>
2022-11-09 23:37:51 +0530

    Minor refactoring

--- src/com/codecafe/concurrency/_volatile/VolatileDemo.java
+++ src/com/codecafe/concurrency/_volatile/VolatileDemo.java
@@ -2,7 +2,7 @@ package com.codecafe.concurrency._volatile;
 
 class Task implements Runnable {
 
-  volatile boolean isRunning = true;
+  private volatile boolean isRunning = true;
 
   @Override
   public void run() {
